2a40d66426d342c054b76df731c562c443b28225
Norrico Gerald <norricobiason31@gmail.com>
2022-10-25 22:18:37 +0800

    theme setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import {ColorModeContext, useMode} from './utils/theme'
+
 function App() {
+	const [theme, colorMode] = useMode()
 	return (
-		<div className='app'>Hello JS</div>
+		<ColorModeContext.Provider value={colorMode}>
+			<ThemeProvider theme={theme}>
+				<CssBaseline />
+				<div className='app'>
+					<main className="content"></main>
+				</div>
+			</ThemeProvider>
+		</ColorModeContext.Provider>
 	)
 }
 
 export default App;
+
+// 36:36 video time
