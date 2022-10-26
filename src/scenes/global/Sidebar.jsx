98e527e29d8ef87f3d44af41bcee5501d81b17a1
Norrico Gerald <norricobiason31@gmail.com>
2022-10-26 22:47:28 +0800

    switch branch to sidebar

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -1,4 +1,9 @@
-import React from 'react'
+import "react-pro-sidebar/dist/css/styles.css"
+import {useState} from 'react'
+import { ProSidebar, Menu, MenuItem } from 'react-pro-sidebar'
+import { Box, IconbButton, Typography, useTheme } from "@mui/system"
+import { Link } from "react-router-dom"
+import { tokens } from './../../utils/theme'
 
 const Sidebar = () => {
     return (
