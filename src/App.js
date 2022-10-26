17cadbd58f941f14049dc9fb637322613c691bc2
Norrico Gerald <norricobiason31@gmail.com>
2022-10-26 22:44:48 +0800

    default route

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import {useState} from 'react'
 import { CssBaseline, ThemeProvider } from '@mui/material'
 import {Routes, Route} from 'react-router-dom'
 import {ColorModeContext, useMode} from './utils/theme'
@@ -17,25 +18,27 @@ import Calendar from "./scenes/calendar/calendar"
 
 function App() {
 	const [theme, colorMode] = useMode()
+	const [isSidebar, setIsSidebar] = useState(true)
 	return (
 		<ColorModeContext.Provider value={colorMode}>
 			<ThemeProvider theme={theme}>
 				<CssBaseline />
 				<div className='app'>
+					<Sidebar />
 					<main className="content">
 						<Topbar />
 						<Routes>
 							<Route path="/" element={<Dashboard />} />
-							<Route path="/team" element={<Team />} />
-							<Route path="/contacts" element={<Contacts />} />
-							<Route path="/invoices" element={<Invoices />} />
-							<Route path="/form" element={<Form />} />
-							<Route path="/bar" element={<Bar />} />
-							<Route path="/pie" element={<Pie />} />
-							<Route path="/line" element={<Line />} />
-							<Route path="/faq" element={<FAQ />} />
-							<Route path="/calendar" element={<Calendar />} />
-							<Route path="/geography" element={<Geography />} />
+							{/* <Route path="/team" element={<Team />} /> */}
+							{/* <Route path="/contacts" element={<Contacts />} /> */}
+							{/* <Route path="/invoices" element={<Invoices />} /> */}
+							{/* <Route path="/form" element={<Form />} /> */}
+							{/* <Route path="/bar" element={<Bar />} /> */}
+							{/* <Route path="/pie" element={<Pie />} /> */}
+							{/* <Route path="/line" element={<Line />} /> */}
+							{/* <Route path="/faq" element={<FAQ />} /> */}
+							{/* <Route path="/calendar" element={<Calendar />} /> */}
+							{/* <Route path="/geography" element={<Geography />} /> */}
 						</Routes>
 					</main>
 				</div>
