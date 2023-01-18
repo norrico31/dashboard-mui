bb2765c36e2da128318393d8570342896f48827d
norrico31 <norricobiason31@gmail.com>
2023-01-18 23:58:40 +0800

    calendar sidebar events

--- src/scenes/calendar/index.jsx
+++ src/scenes/calendar/index.jsx
@@ -29,7 +29,19 @@ export default function Calendar() {
 		}
 	}
 
+	const handleEventClick = (selected) => {
+		if (window.confirm(`Are you sure you want to delete the event ${selected.event.title}`)) selected.event.remove()
+	}	
+
 	return (
-		<div>index</div>
+		<Box m='20px'>
+			<Header title='CALENDAR' subtitle='Full Calendar Interactive Page' />
+			<Box display='flex' justifyContent='space-between'>
+				{/* CALENDAR SIDEBAR */}
+				<Box flex='1 1 20%' backgroundColor={colors.primary[400]} p='15px' borderRadius='4px'>
+
+				</Box>
+			</Box>
+		</Box>
 	)	
 }
