9af0d034824957055b30a61979c5bbb256137959
norrico31 <norricobiason31@gmail.com>
2023-01-23 22:28:48 +0800

    nivo line chart

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Invoices from "./scenes/invoices"
 import Form from "./scenes/form"
 import Calendar from "./scenes/calendar"
 import Bar from "./scenes/bar"
-// import Line from "./scenes/line"
+import Line from "./scenes/line"
 import Pie from "./scenes/pie"
 import FAQ from "./scenes/faq"
 // import Geography from "./scenes/geography"
@@ -36,7 +36,7 @@ function App() {
 							<Route path="/calendar" element={<Calendar />} />
 							<Route path="/bar" element={<Bar />} />
 							<Route path="/pie" element={<Pie />} />
-							{/* <Route path="/line" element={<Line />} /> */}
+							<Route path="/line" element={<Line />} />
 							<Route path="/faq" element={<FAQ />} />
 							{/* <Route path="/geography" element={<Geography />} /> */}
 						</Routes>
