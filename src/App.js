a536b5952a0eb9d5c215237e0af322c1a59f9d22
norrico31 <norricobiason31@gmail.com>
2023-01-23 22:34:42 +0800

    nivo geography

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import Bar from "./scenes/bar"
 import Line from "./scenes/line"
 import Pie from "./scenes/pie"
 import FAQ from "./scenes/faq"
-// import Geography from "./scenes/geography"
+import Geography from "./scenes/geography"
 
 function App() {
 	const [theme, colorMode] = useMode()
@@ -38,7 +38,7 @@ function App() {
 							<Route path="/pie" element={<Pie />} />
 							<Route path="/line" element={<Line />} />
 							<Route path="/faq" element={<FAQ />} />
-							{/* <Route path="/geography" element={<Geography />} /> */}
+							<Route path="/geography" element={<Geography />} />
 						</Routes>
 					</main>
 				</div>
