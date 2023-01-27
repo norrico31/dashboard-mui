cf685a8b7e14b2838f685fb7541c0eed760b5d1c
norrico31 <norricobiason31@gmail.com>
2023-01-28 01:07:15 +0800

    row 3 done

--- src/App.js
+++ src/App.js
@@ -49,4 +49,4 @@ function App() {
 
 export default App
 
-// 3:39:35
+// 3:45:10
--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -2,7 +2,7 @@ import { Box, Button, IconButton, Typography, useTheme } from '@mui/material'
 import { tokens } from '../../utils/theme'
 import Header from '../../components/Header'
 import { mockTransactions } from '../../data/mockData'
-import DownloadOutlined from '@mui/icons-material/DownloadOutlined'
+import DownloadOutlinedIcon from '@mui/icons-material/DownloadOutlined'
 import EmailIcon from '@mui/icons-material/Email'
 import PointOfSaleIcon from '@mui/icons-material/PointOfSale'
 import PersonAddIcon from '@mui/icons-material/PersonAdd'
@@ -25,7 +25,7 @@ const Dashboard = () => {
                     <Button
                         sx={{ backgroundColor: colors.blueAccent[700], color: colors.grey[100], fontSize: '14px', fontWeight: 'bold', padding: '10px 20px' }}
                     >
-                        <DownloadOutlined sx={{ mr: '10px' }} />
+                        <DownloadOutlinedIcon sx={{ mr: '10px' }} />
                         Download Reports
                     </Button>
                 </Box>
@@ -96,6 +96,66 @@ const Dashboard = () => {
                         increase="+43%"
                         icon={<TrafficIcon sx={{ color: colors.greenAccent[600], fontSize: "26px" }} />}
                     />
+                </Box>
+
+                {/* ROW 2 */}
+                <Box
+                    gridColumn='span 8'
+                    gridRow='span 2'
+                    backgroundColor={colors.primary[400]}
+                >
+                    <Box mt='25px' p='0 30px' display='flex' justifyContent='space-between' alignItems='center'>
+                        <Box>
+                            <Typography variant='h5' fontWeight='600' color={colors.grey[100]}>Revenue Generated</Typography>
+                            <Typography variant='h3' fontWeight='bold' color={colors.greenAccent[500]}>$59,342.42</Typography>
+                        </Box>
+                        <Box>
+                            <IconButton>
+                                <DownloadOutlinedIcon sx={{fontSize: '26px', color: colors.greenAccent[500]}} />
+                            </IconButton>
+                        </Box>
+                    </Box>
+                    <Box height='250px' mt='-20px'>
+                        <LineChart isDashboard />
+                    </Box>
+                </Box>
+
+                {/* TRANSACTIONS */}
+                <Box gridColumn='span 4' gridRow='span 2' backgroundColor={colors.primary[400]} overflow="auto">
+                    <Box display='flex' justifyContent='space-between' alignItems='center' borderBottom={`4px solid ${colors.primary[500]}`} colors={colors.grey[100]} p='15px'>
+                        <Typography color={colors.grey[100]} variant="h5" fontWeight="600">Recent Transactions</Typography>
+                    </Box>
+                    {mockTransactions.map((trans, i) => (
+                        <Box
+                            key={`${trans.txId}-${i}`}
+                            display="flex"
+                            justifyContent="space-between"
+                            alignItems="center"
+                            borderBottom={`4px solid ${colors.primary[500]}`}
+                            p="15px"
+                        >
+                            <Box>
+                            <Typography
+                            color={colors.greenAccent[500]}
+                            variant="h5"
+                            fontWeight="600"
+                            >
+                            {trans.txId}
+                            </Typography>
+                            <Typography color={colors.grey[100]}>
+                            {trans.user}
+                            </Typography>
+                        </Box>
+                        <Box color={colors.grey[100]}>{trans.date}</Box>
+                        <Box
+                            backgroundColor={colors.greenAccent[500]}
+                            p="5px 10px"
+                            borderRadius="4px"
+                        >
+                            ${trans.cost}
+                        </Box>
+                        </Box>
+                    ))}
                 </Box>
             </Box>
 
